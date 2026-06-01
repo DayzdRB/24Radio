19536637c4efb1a40cacfb4347c39a2e0650506a
DayzdRB <2008trev@gmail.com>
2026-05-31 23:14:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+let frequencies = [];
 
+async function loadfrequencies()
+{
+  try
+  {
+    const response = await fetch("freq.json");
+    const data = await response.json();
+    frequencies = data;
+    console.log("frequencies loaded:", frequencies);
+    console.log("total entries: ", frequencies.length);
+    
+  }catch(error){
+    console.error("Error loading freq.json:", error);
+  }
+}
